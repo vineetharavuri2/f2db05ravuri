eabb3128ca994df29426877247a27ee2ebbc451b
vineetharavuri2 <vineetharavuri2@gmail.com>
2022-11-18 01:22:57 -0600

    modified update and detail

--- models/Account.js
+++ models/Account.js
@@ -11,4 +11,4 @@ accountSchema.plugin(passportLocalMongoose);
  
 // We export the Schema to avoid attaching the model to the 
 // default mongoose connection. 
-module.exports = mongoose.model("Account", accountSchema); 
+module.exports = mongoose.model("Account",accountSchema); 
