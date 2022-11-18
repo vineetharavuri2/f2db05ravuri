6f35b005cce9fdec1be2478fb6fba3c9d386a769
vineetharavuri2 <vineetharavuri2@gmail.com>
2022-11-18 00:34:23 -0600

    modified update and detail

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var electronics = require('./models/electronics');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+)); 
+
 require('dotenv').config();
 const connectionString =
   process.env.MONGO_CON
@@ -14,27 +31,42 @@ mongoose.connect(connectionString,
     useUnifiedTopology: true
   });
 
-//Get the default connection 
-var db = mongoose.connection;
-
-//Bind connection to error event  
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function () {
-  console.log("Connection to DB succeeded")
-});
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var electronicsRouter = require('./routes/electronics');
 var gridbuildRouter = require('./routes/gridbuild');
 var selectorRouter = require('./routes/selector');
-var resourceRouter = require('./routes/resource');
+var resourceRouter = require("./routes/resource");
+var electronics = require("./models/electronics");
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/electronics', electronicsRouter);
+app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
+
 
 async function recreateDB() {
   // Delete everything 
@@ -59,22 +91,18 @@ async function recreateDB() {
   });
 }
 
+
 let reseed = true;
 if (reseed) { recreateDB(); }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/electronics', electronicsRouter);
-app.use('/gridbuild', gridbuildRouter);
-app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
-
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
